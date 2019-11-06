03e5ef148ee82162651d5cfc19d3b7de32f5d1bb
Len Hughes <tug49816@temple.edu>
2019-11-06 17:43:32 -0500

    file changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
 "use strict";
 
 /* SOME CONSTANTS */
-var endpoint01 = "http://ec2-3-15-38-21.us-east-2.compute.amazonaws.com";
+//var endpoint01 = "http://ec2-3-15-38-21.us-east-2.compute.amazonaws.com";
+var endpoint01 = "http://13.59.4.251:8222";
 localStorage.usertoken = 0;
 localStorage.lastnavlink = '';
 
+//user variable declarations
+var username;
+var password;
+var firstname;
+var lastname;
+var phonenumber;
+var creditrating;
+var income;
+
 /* SUPPORTING FUNCTIONS */
+var logoutController = function(){
+localStorage.usertoken = 0;
+username = "";
+password = "";
+firstname = "";
+lastname = "";
+phonenumber = "";
+creditscore = "";
+income = "";
+
+$('.secured').removeClass('unlocked');
+$('.secured').addClass('locked');
+
+$("#renterid").val(" ");
+
+$(".content-wrapper").hide();
+$("#div-welcome").show();
+};
+
 
 var navigationControl = function(the_link){
 
@@ -27,32 +56,120 @@ var navigationControl = function(the_link){
 	$("html, body").animate({ scrollTop: "0px" }); /* scroll to top of page */
 	$(".navbar-collapse").collapse('hide'); /* explicitly collapse the navigation menu */
 
-} /* end navigation control */
+}; /* end navigation control */
+
 
 var loginController = function(){
 	//go get the data off the login form
+	
+
 	var the_serialized_data = $('#form-login').serialize();
-	var url = endpoint01 + '/auth/';
-	$.getJSON(url,the_serialized_data,function(data){
-		//console.log(data);
-		if (typeof data === 'string'){
-			localStorage.usertoken = 0; // login failed.  Set usertoken to it's initial value.
-			$('#login_message').html(data);
-			$('#login_message').show();
-		} else {
+	var urltext = endpoint01 + '/renter';
+	$.ajax({
+		url: urltext,
+		data: the_serialized_data,
+		type: 'GET',
+		success: function(result){
+			console.log(result);
 			$('#login_message').html('');
 			$('#login_message').hide();
-			localStorage.usertoken = data['user_id']; //login succeeded.  Set usertoken.
+			localStorage.usertoken = result['renterid']; //login succeeded.  Set usertoken.
+			username = result['username'];
+			password = result['password'];
+			firstname = result['firstname'];
+			lastname = result['lastname'];
+			phonenumber = result['phone'];
+			creditrating = result['creditrating'];
+			income = result['income'];
+
 			$('.secured').removeClass('locked');
 			$('.secured').addClass('unlocked');
+			$('#renterid').val(localStorage.usertoken);
+
 			$('#div-login').hide();
-			$('#div-ABC').show();
+			$('#div-dashboard').show();
+		
+		},
+		error: function(result){
+			console.log(result);
+			localStorage.usertoken = 0; // login failed.  Set usertoken to it's initial value.
+			$('#login_message').html(result['responseJSON']);
+			$('#login_message').show();
 		}
 	});
+
 	//scroll to top of page
 	$("html, body").animate({ scrollTop: "0px" });
 };
 
+var accountCreate = function(){
+	var the_serialized_data = $('#form-signup').serialize();
+	var urltext = endpoint01 + '/renter';
+
+	$.ajax({
+		url: urltext,
+		data: the_serialized_data,
+		type: 'POST',
+		success: function(result){
+			console.log(result);
+			$('#signup_message').html('');
+			$('#signup_message').hide();
+			
+			localStorage.usertoken = result['renterid']; //login succeeded.  Set usertoken.
+			$('.secured').removeClass('locked');
+			$('.secured').addClass('unlocked');
+			$('#renterid').val(localStorage.usertoken);
+
+			$(".content-wrapper").hide();
+			$("#div-signupconfirm").show();
+		},
+		error: function(result){
+			console.log(result);
+			$('#signup_message').html(result['responseJSON']);
+			$('#signup_message').show();
+		}
+
+	});
+};
+
+var profileUpdate = function(){
+	console.log(username);
+	//$('#updateusername').val(username);
+	/*
+password = "";
+firstname = "";
+lastname = "";
+phonenumber = "";
+creditscore = "";
+income = "";
+*/
+
+	var the_serialized_data = $('#form-update').serialize();
+	var urltext = endpoint01 + '/renter';
+	var updateData= 'renterid=' + localStorage.usertoken + '&' + the_serialized_data;
+	console.log(updateData);
+
+	$.ajax({
+		url: urltext,
+		data: updateData,
+		type: 'PUT',
+		success: function(result){
+			console.log(result);
+			$('#update_message').html('');
+			$('#update_message').hide();
+
+			$("#form-update").hide();
+			$('#div-updatesuccess').show();
+
+		},
+		error: function(result){
+			console.log(result);
+			//$('#update_message').html(result['responseJSON']);
+			$('#update_message').html('Server Error');
+			$('#update_message').show();
+		}
+	});
+}
 
 //document ready section
 $(document).ready(function (){
@@ -76,10 +193,26 @@ $(document).ready(function (){
 		
 	/* what happens if the login button is clicked? */
 	$('#btnLogin').click(function(){
-		//loginController();
+		if ($('#username').val() == "" && $('#password').val() ==""){
+			//alert("username blank");
+			$('#login_message').html("Please enter a username and password");
+				$('#login_message').show();
+				return;
+			}if ($('#username').val() == ""){
+			//alert("username blank");
+			$('#login_message').html("Please enter a username");
+				$('#login_message').show();
+				return;
+		} if ($('#password').val() ==""){
+			$('#login_message').html("Please enter a password");
+				$('#login_message').show();
+				return;
+		} else {
+		loginController();
+		}
 		
-		$(".content-wrapper").hide();
-		$("#div-dashboard").show();
+		//$(".content-wrapper").hide();
+		//$("#div-dashboard").show();
 	});
 
 	// signup button click
@@ -93,9 +226,26 @@ $(document).ready(function (){
 	// signup2 button click
 	$('#btnSignup2').click(function(){
 		//hide all content-wrapper divs
-		$(".content-wrapper").hide();
+		if ($('#signupusername').val() == "" && $('#signuppassword').val() ==""){
+			//alert("username blank");
+			$('#signup_message').html("Please enter a username and password");
+				$('#signup_message').show();
+				return;
+		}if ($('#signupusername').val() == ""){
+			//alert("username blank");
+			$('#signup_message').html("Please enter a username");
+				$('#signup_message').show();
+				return;
+		} if ($('#signuppassword').val() ==""){
+			$('#signup_message').html("Please enter a password");
+				$('#signup_message').show();
+				return;
+		} else {
+		accountCreate();
+		//$(".content-wrapper").hide();
 		//show next "page"
-		$("#div-signupconfirm").show();
+		//$("#div-signupconfirm").show();
+		}
 	});
 
 	// signin2 button click
@@ -105,15 +255,24 @@ $(document).ready(function (){
 		//show next "page"
 		$("#div-login").show();
 	});
+
+	// signin button click
+	$('#btnSignin').click(function(){
+		//hide all content-wrapper divs
+		$(".content-wrapper").hide();
+		//show next "page"
+		$("#div-login").show();
+	});
 	
-	//list click
-	$('.list-group').click(function(){
+	//propertysearch click
+	$('#property-search').click(function(){
 		//hide all content-wrapper divs
 		$(".content-wrapper").hide();
 		//show next "page"
 		$("#div-propertysearch").show();
 	});
 
+	//search click
 	$('#btnSearch').click(function(){
 		if ($('#city').val()==""){
 			$('#searchmessage').show();
@@ -125,5 +284,42 @@ $(document).ready(function (){
 		$("#div-propertylist").show();
 		}
 	});
+
+	//logout link
+	$('#dashboard-logout').click(function(){
+		//alert("logout clicked");
+		logoutController();
+	});
+
+	//dashboard update
+	$('#dashboard-update').click(function(){
+		//hide all content-wrapper divs
+		$(".content-wrapper").hide();
+		//show next "page"
+		$("#div-updateprofile").show();
+		$("#div-updatesuccess").hide();
+		$('#formupdate').show();
+
+		$('#updateusername').val(username);
+		$('#updatepassword').val(password);
+		$('#updatefirstname').val(firstname);
+		$('#updatelastname').val(lastname);
+		$('#updatephonenumber').val(phonenumber);
+		$('#updatecreditrating').val(creditrating);
+		$('#updateincome').val(income);
+
+	});
 		
+	//update button
+	$('#btnUpdate').click(function(){
+		profileUpdate();
+	})
+
+	//return to dashboard after successful update
+	$('#btnDashboardReturn').click(function(){
+		//hide all content-wrapper divs
+		$(".content-wrapper").hide();
+		//show next "page"
+		$("#div-dashboard").show();
+	})
 }); /* end the document ready event*/
